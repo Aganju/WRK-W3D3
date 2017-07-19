a60431cb951345dccd390a9ed426b8d02990a135
App Academy Student <alexkornfeld3000@hotmail.com>
2017-07-19 14:27:33 -0400

    implement associations, css

---
+++ associations/app/models/enrollment.rb
@@ -0,0 +1,13 @@
+class Enrollment < ActiveRecord::Base
+  validates :student_id, :course_id, presence: true
+
+  belongs_to :user,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :User
+
+  belongs_to :course,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Course
+end
